256e79fd927af3711bde4bef145c202cd2641e82
Felix4President <wink.fel@gmail.com>
2022-06-07 09:04:12 +0200

    New Map an Intercations
    added interaction points and websites

---
+++ Text6.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Text6" tilewidth="32" tileheight="32" tilecount="81" columns="9">
+ <image source="C:/Download/ikt00629584/288h/Zeichenfläche 1 Kopie 3.png" width="298" height="288"/>
+</tileset>
